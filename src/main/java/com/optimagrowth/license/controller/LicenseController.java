0915324b604584c2f9f85e60367d91eaf974b777
PorcoGalliard <nanasuryana335@gmail.com>
2023-07-09 10:29:42 +0700

    feat: implement internationalization and change api root name

--- src/main/java/com/optimagrowth/license/controller/LicenseController.java
+++ src/main/java/com/optimagrowth/license/controller/LicenseController.java
@@ -3,11 +3,14 @@ package com.optimagrowth.license.controller;
 import com.optimagrowth.license.model.License;
 import com.optimagrowth.license.service.LicenseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Locale;
+
 @RestController
-@RequestMapping(value = "v1/organization/{organizationId}/license")
+@RequestMapping(value = "api/v1/organization/{organizationId}/license")
 
 public class LicenseController {
 
@@ -26,9 +29,10 @@ public class LicenseController {
     @PostMapping(value = "/{licenseId}")
     public ResponseEntity<String> createLicense(
             @PathVariable("organizationId") String organizationId,
-            @RequestBody License request
-    ) {
-        return ResponseEntity.ok(licenseService.createLicense(request, organizationId));
+            @RequestBody License request,
+            @RequestHeader(value = "Accept-Language", required = false) Locale locale
+            ) {
+        return ResponseEntity.ok(licenseService.createLicense(request, organizationId, locale));
     }
 
     @PutMapping(value = "/{licenseId}")
--- src/main/java/com/optimagrowth/license/service/LicenseService.java
+++ src/main/java/com/optimagrowth/license/service/LicenseService.java
@@ -1,12 +1,20 @@
 package com.optimagrowth.license.service;
 
 import com.optimagrowth.license.model.License;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 
+import java.util.Locale;
 import java.util.Random;
 
 @Service
 public class LicenseService {
+
+    @Qualifier("messageSource")
+    @Autowired
+    MessageSource messages;
 
     public License getLicense(String licenseId, String organizationId) {
         License license = new License();
@@ -20,11 +28,11 @@ public class LicenseService {
         return license;
     }
 
-    public String createLicense(License license, String organizationId) {
+    public String createLicense(License license, String organizationId, Locale locale) {
         String responseMessage = null;
         if (license != null) {
             license.setOrganizationId(organizationId);
-            responseMessage = String.format("This is the post and the object is: %s", license.toString());
+            responseMessage = String.format(messages.getMessage("license.create.message", null, locale), license.toString());
         }
 
         return responseMessage;
@@ -34,7 +42,7 @@ public class LicenseService {
         String responseMessage = null;
         if (license != null) {
             license.setOrganizationId(organizationId);
-            responseMessage = String.format("This is the put and the object is: %s", license.toString());
+            responseMessage = String.format(messages.getMessage("license.update.message", null, null), license.toString());
         }
 
         return responseMessage;
