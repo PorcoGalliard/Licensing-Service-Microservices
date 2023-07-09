01d7e6c50ad534de891d326d860b983832b2deeb
PorcoGalliard <nanasuryana335@gmail.com>
2023-07-09 16:36:27 +0700

    feat: implement HATEOAS on license service

--- src/main/java/com/optimagrowth/license/controller/LicenseController.java
+++ src/main/java/com/optimagrowth/license/controller/LicenseController.java
@@ -3,9 +3,10 @@ package com.optimagrowth.license.controller;
 import com.optimagrowth.license.model.License;
 import com.optimagrowth.license.service.LicenseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 import java.util.Locale;
 
@@ -23,6 +24,21 @@ public class LicenseController {
             @PathVariable("licenseId") String licenseId
     ) {
         License license = licenseService.getLicense(licenseId, organizationId);
+
+        license.add(linkTo(methodOn(LicenseController.class)
+                                .getLicense(organizationId, license.getLicenseId()))
+                                .withSelfRel(),
+                        linkTo(methodOn(LicenseController.class)
+                                .createLicense(organizationId, license, null))
+                                .withRel("createLicense"),
+                        linkTo(methodOn(LicenseController.class)
+                                .updateLicense(organizationId, license))
+                                .withRel("updateLicense"),
+                        linkTo(methodOn(LicenseController.class)
+                                .deleteLicense(organizationId, license.getLicenseId()))
+                                .withRel("deleteLicense")
+        );
+
         return ResponseEntity.ok(license);
     }
 
